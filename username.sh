64ef535b7bbab56202470809290b8f5f8d2909ef
Andrew Dillon <adillon@chapman.edu>
2020-11-13 12:06:30 -0800

    Fixed the repromt message

--- username.sh
+++ username.sh
@@ -9,8 +9,11 @@ echo "Enter Username here: "
 read NAME
 while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
+	echo "Sorry, this Username is invald."
+	echo "Username must only contain lowercase letters, digits, and underscores."
+	echo "It must start with a letter."
+	echo "Username must be between 3 and 12 characters long."
+	echo "Enter Username here:"
 	read NAME
 done
 echo "Thank you"
