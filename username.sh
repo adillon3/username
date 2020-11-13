a9d84f60f61a03466b7f264fbb34b713bf4f6f56
Andrew Dillon <adillon@chapman.edu>
2020-11-13 11:21:24 -0800

    Updated userprompts

--- username.sh
+++ username.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 # username.sh
 # Andrew Dillon
-echo "Enter a Username"
-echo "Username must only contain lowercase letters, digits, and underscores"
-echo "It must start with a letter:"
+echo "-Username-"
+echo "Username must only contain lowercase letters, digits, and underscores."
+echo "It must start with a letter."
+echo "Username must be between 3 and 12 characters long."
+echo "Enter Username here: "
 read NAME
 while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
